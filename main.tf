e20d0880330e6f0dd2bd54cd3828144854875710
yathish12 <103912830+yathish12@users.noreply.github.com>
2025-01-20 16:20:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "us-east-1"
+    region = var.region
     access_key = "AKIA6ODU32WASKYFAZEO"
     secret_key = "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"
 }
 
 resource "aws_instance" "myec2" {
-    ami = "ami-04b4f1a9cf54c11d0"
-    instance_type = "t2.micro"
+    ami = var.ami
+    instance_type = var.instance_type
 }
